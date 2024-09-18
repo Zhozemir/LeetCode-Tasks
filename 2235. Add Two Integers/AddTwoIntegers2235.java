50920b2b6c80dbbea232a84030c9eba20bc39293
Zhozemir Kushev <147644881+Zhozemir@users.noreply.github.com>
2024-09-18 12:05:07 +0300

    Add files via upload

---
+++ 2235. Add Two Integers/AddTwoIntegers2235.java
@@ -0,0 +1,19 @@
+package BeginnerTasks;
+
+public class AddTwoIntegers2235 {
+
+    public static int sum(int num1, int num2) {
+
+        return num1 + num2;
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(sum(3,4));
+
+    }
+
+}
+
+
