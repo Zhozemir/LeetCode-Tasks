95b0b2b68ded23cd82cac56f6abc9867899712ff
Zhozemir Kushev <147644881+Zhozemir@users.noreply.github.com>
2024-11-24 09:55:46 +0200

    Update 387.FirstUniqueCharacterInAString.cpp

--- 387.FirstUniqueCharacterInAString.cpp
+++ 387.FirstUniqueCharacterInAString.cpp
@@ -3,24 +3,19 @@
 #include <map>
 
 int firstUniqChar(const std::string& s) {
+    
     std::map<char, int> freqMap;
 
-    for (char c : s) {
+    for (char c : s)
         freqMap[c]++;
-    }
-
+    
     for (size_t i = 0; i < s.size(); ++i) {
-        if (freqMap[s[i]] == 1) {
+        
+        if (freqMap[s[i]] == 1)
             return i;
-        }
+
     }
 
     return -1;
-}
-
-int main() {
-    std::string s = "loveleetcode";
-    std::cout << "The index of the first unique character is: " << firstUniqChar(s) << std::endl;
-
-    return 0;
+    
 }
