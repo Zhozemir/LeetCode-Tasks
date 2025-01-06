9045f1493082be0649b8d086cbe843efc7ab3cbd
Zhozemir Kushev <147644881+Zhozemir@users.noreply.github.com>
2025-01-06 12:45:16 +0200

    Update 451.SortCharactersByFrequency.cpp

--- 451.SortCharactersByFrequency.cpp
+++ 451.SortCharactersByFrequency.cpp
@@ -38,6 +38,52 @@ std::string frequencySort(std::string s) {
 
 }
 
+//struct CompareBySecond {
+//
+//	bool operator()(const std::pair<char, int>& a, const std::pair<char, int>& b) {
+//
+//		return a.second < b.second;
+//
+//	}
+//
+//};
+//
+//std::string frequencySort(std::string s) {
+//
+//	std::unordered_map<char, int> hm;
+//
+//	std::priority_queue<std::pair<char, int>, std::vector<std::pair<char, int>>, CompareBySecond> pq;
+//
+//	for (const auto& el : s)
+//		hm[el]++;
+//
+//	for (const auto& pair : hm)
+//		pq.push({ pair.first, pair.second });
+//
+//	std::string newStr;
+//
+//	while (!pq.empty()) {
+//
+//		auto currentPair = pq.top();
+//
+//		int count = currentPair.second;
+//		char ch = currentPair.first;
+//
+//		while (count != 0) {
+//
+//			newStr.push_back(ch);
+//			count--;
+//
+//		}
+//
+//		pq.pop();
+//
+//	}
+//
+//	return newStr;
+//
+//}
+
 int main() {
 
 	std::string s = "Aabb";
