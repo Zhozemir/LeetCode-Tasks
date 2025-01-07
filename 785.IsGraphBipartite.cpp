756dcd0a48cddbfa2ab5ed3e6b92734be496aeb5
Zhozemir Kushev <147644881+Zhozemir@users.noreply.github.com>
2025-01-08 00:07:50 +0200

    Add files via upload

---
+++ 785.IsGraphBipartite.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+bool isBipartite(std::vector<std::vector<int>>& graph) {
+
+    int n = graph.size();
+    std::vector<int> color(n, -1); 
+
+    for (int i = 0; i < n; i++) {
+
+        if (color[i] == -1) { 
+
+            std::queue<int> q;
+            q.push(i);
+            color[i] = 0; 
+
+            while (!q.empty()) {
+
+                int node = q.front();
+                q.pop();
+
+                for (int neighbor : graph[node]) {
+
+                    if (color[neighbor] == -1) { 
+
+                        color[neighbor] = 1 - color[node]; 
+                        q.push(neighbor);
+
+                    }
+                    else if (color[neighbor] == color[node])
+                        return false;
+                    
+                }
+            }
+        }
+    }
+
+    return true; 
+}
